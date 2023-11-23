747baa757e3ebeca03972a532d38cccdaffaf8d0
Sri <techkonnecttraining@gmail.com>
2023-11-22 20:04:36 -0600

    Added code changes for spring boot project

---
+++ src/test/java/com/techkonnect/java/techkonnectjava/TechKonnectJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techkonnect.java.techkonnectjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechKonnectJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
