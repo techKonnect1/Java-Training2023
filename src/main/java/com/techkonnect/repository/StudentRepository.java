0b707c4cf357386219803eb44fa5da56b813e631
Sri <techkonnecttraining@gmail.com>
2023-12-13 20:25:29 -0600

    Merge conflit

--- src/main/java/com/techkonnect/repository/StudentRepository.java
+++ src/main/java/com/techkonnect/repository/StudentRepository.java
@@ -1,10 +1,14 @@
 package com.techkonnect.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.techkonnect.entity.Student;
 
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+	
+	
+	List<Student> findByFirstNameAndLastName(Student student);
     // Additional custom queries can be defined here if needed
 }
